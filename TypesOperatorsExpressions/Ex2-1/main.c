fc9aa9363f96fdddbfad521c310bba45ba296d8e
Rohit Kumar <rohitsinha050481@gmail.com>
2022-04-15 09:32:46 +0530

    InitialCommit

---
+++ TypesOperatorsExpressions/Ex2-1/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(int argV,char** argC)
+{
+
+
+}
+
---
+++ TypesOperatorsExpressions/Ex2-1/range.h
@@ -0,0 +1,10 @@
+#include<float.h>
+#include<limits.h>
+
+//function declarations
+unsigned long RangeFloat();
+unsigned long RangeInt();
+unsigned long RangeShort();
+unsigned long RangeUnsignedFloat();
+unsigned long RangeUnsignedInt();
+unsigned long RangeUnsignedShort();
