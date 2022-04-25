b4671b31f784211d356eb4b789cf8c5794cbdd82
rohit kumar <rohitsinha050481@gmail.com>
2022-04-25 11:10:06 +0530

    itoa

---
+++ ControlFlow/atoi/atoi/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  atoi
+//
+//  Created by rkumar43 on 25/04/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
--- ControlFlow/itoa/itoa/main.c
+++ ControlFlow/itoa/itoa/main.c
@@ -6,9 +6,50 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void itoa(int n,char * szBuff);
+void reverse(char* szBuff);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    char buffer[1024];
+    int input;
+    scanf("%d",&input);
+    itoa(input, buffer);
+    printf("Number string is %s \n",buffer);
+    reverse(buffer);
+    printf("Reversed buffer string is %s \n",buffer);
     return 0;
+}
+
+void itoa(int n,char * szBuff)
+{
+    int temp = n;
+    printf("Number entered is %d \n",n);
+    do{
+//this generates the numbers in reverse order
+        temp = n % 10;
+        *szBuff = temp + '0';
+        szBuff++;
+    }
+    while((n/=10) > 0);
+    *szBuff = '\0';
+}
+
+//this reverses the string
+void reverse(char * szBuff)
+{
+    int len = (int) strlen(szBuff);
+    int count = 0;
+    do{
+        char temp;
+        temp = szBuff[count];
+        szBuff[count] = szBuff[len - (count+1)];
+        szBuff[len - (count+1)] = temp;
+        count++;
+    }
+    while(count<len/2);
+    szBuff[len]='\0';
 }
